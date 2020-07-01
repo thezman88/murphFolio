13a81f7b3ebb793c8ff44749edc13fb156579690
Zachary Cohen <zacharycohen@Zacharys-MBP.home>
2020-07-01 13:54:40 -0400

    filtered portfolio gallery under construction

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,11 @@
-console.log('this works');
-
 let downArrow = document.getElementById('downArrow');
 
 downArrow.addEventListener('click', function(){
-    console.log('clicked');
     this.scrollIntoView({behavior:'smooth', #latest})
-})
+});
+
+
+
+    
+
+
